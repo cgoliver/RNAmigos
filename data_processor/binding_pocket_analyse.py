ef2d4c81077508d0b65e065477716fff65d36590
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2020-01-17 10:16:49 -0500

    binding pocket extractors

---
+++ data_processor/binding_pocket_analyse.py
@@ -0,0 +1,154 @@
+""""
+Analyzes PDB structures with ligands and counts the number of
+RNA vs Protein residues at various sphere radii.
+
+The output of this script can be used to select binding pockets.
+"""
+import os
+import sys
+import pickle
+import multiprocessing
+
+import numpy as np
+
+import pandas as pd
+from Bio.PDB import *
+
+
+def ligand_center(residue):
+    return np.mean(np.array([atom.coord for atom in residue.get_atoms()]), axis=0)
+
+def is_valid_ligand(ligand_residue):
+    #no ions
+    invalids = ['HOH', 'NCO', 'SO4', 'EPE', 'OHX', 'MPD']
+    if ligand_residue.resname in invalids or len(ligand_residue.resname) != 3:
+        return False
+    return True
+
+def pocket_res_count(struct, ligand_residue, radius=12):
+    """
+        Isolate residues around a ligand for a range of radii and counts
+        the number of RNA residues and Protein residues in the radius.
+        
+        Arguments:
+            struct (Bio.PDB.Structure): BioPython object for full structure.
+            ligand_residue (Bio.PDB.Residue): BioPython residue object for ligand.
+            radius (int): Largest radius around ligand to accept.
+
+        Returns:
+            pocket_rescount (list): list of protein/rna residue counts for each cutoff.
+    """
+    #get all residues within cutoff
+    kd = NeighborSearch(list(struct.get_atoms()))
+    center = ligand_center(ligand_residue)
+
+
+    #list of rna and protein concentrations in region (cutoff, rna_res, protein_res)
+    pocket_rescount = []
+    for c in range(1, radius):
+        pocket = kd.search(ligand_center(ligand_residue), c, level='R')
+
+        num_protein_res, num_rna_res = (0,0)
+        #if there is protein pocket is no bueno
+        for res in pocket:
+            res_name = res.resname.strip()
+            if res_name in ['A', 'U', 'C', 'G']:
+                num_rna_res += 1
+            elif res_name != ligand_residue.resname:
+                num_protein_res += 1
+            else:
+                continue
+        pocket_rescount.append({'cutoff': c, 'rna': num_rna_res, 'protein': num_protein_res})
+
+    return pocket_rescount
+
+def full_struc_analyse(strucpath):
+    """
+    Analyses a full PDB structure for its binding pockets.
+    Counts RNA/Protein residue counts at all binding sites.
+
+    Arguments:
+        strucpath (str): Path to PDB structure.
+    Returns:
+        valid_ligs_info (list): List of pocket info for each ligand.
+                                `[(lig_res_id, {'rna_count' int, 'prot_count', int, 'cutoff', int})]`
+
+    """
+    #load mmCIF structure
+    struc_dict = MMCIF2Dict.MMCIF2Dict(strucpath)
+    #load PDB
+    parser = MMCIFParser(QUIET=True)
+    pdbstruc = parser.get_structure("", strucpath)
+
+    ligand_dict = {}
+    try:
+        ligand_dict['position'] = struc_dict['_pdbx_nonpoly_scheme.pdb_seq_num']
+        ligand_dict['res_name'] = struc_dict['_pdbx_nonpoly_scheme.mon_id']
+        ligand_dict['chain'] = struc_dict['_pdbx_nonpoly_scheme.pdb_strand_id']
+        ligand_dict['unique_id'] = struc_dict['_pdbx_nonpoly_scheme.asym_id']
+
+    except:
+        print("Ligand not detected.")
+
+    num_models = len(pdbstruc)
+    model = pdbstruc[0]
+    try:
+        ligand_df = pd.DataFrame.from_dict(ligand_dict)
+    #pandas complains when dictionary values are not lists
+    #this happens when there is only one ligand in PDB
+    except ValueError:
+        ligand_df = pd.DataFrame(ligand_dict, index=[0])
+
+    ligand_df['position'] = pd.to_numeric(ligand_df['position'])
+    pdbid = os.path.basename(strucpath).split(".")[0]
+    ligand_df['pdbid'] = pdbid
+
+    #check ligands
+    invalid_ligands = 0
+    valid_ligands = []
+    for ligand in ligand_df.itertuples():
+        ligand_res = None
+        #find the residue corresponding to ligand
+        for res in model[ligand.chain].get_residues():
+            if res.id[1] == ligand.position:
+                ligand_res = res
+                if is_valid_ligand(ligand_res):
+                    valid_ligands.append((ligand_res, f"#{'0' if num_models == 1 else '0.1'}", pocket_res_count(model, ligand_res)))
+                else:
+                    invalid_ligands += 1
+    return valid_ligands, strucpath
+
+
+def binding_wrapper(strucpath):
+    try:
+        return full_struc_analyse(strucpath)
+    except KeyboardInterrupt:
+        sys.exit()
+    except Exception as e:
+        print(f"Failed on {strucpath} :: {e}")
+        return None
+
+if __name__ == "__main__":
+    PDB_PATH = os.path.join("..", "data", "all_rna_prot_lig_2019")
+    pdbs = [os.path.join(PDB_PATH, p) for p in os.listdir(PDB_PATH)]
+    num_pdbs = len(pdbs)
+    lig_dict = {}
+
+    todo = iter(pdbs)
+    num_done = 0
+
+    # for result in pool.imap_unordered(get_binding, todo):
+    for result in map(binding_wrapper, todo):
+        if result is None:
+            continue
+        ligs, p = result
+        num_done += 1
+        print(f"done {p}, {num_done} of {num_pdbs}")
+        lig_ids = []
+        for l, model_id, radii in ligs:
+            info = f"{model_id}:{l.get_parent().id}:{l.resname}:{l.id[1]}"
+            print(info)
+            lig_ids.append((info, radii))
+        lig_dict[os.path.basename(p)] = lig_ids
+        # pickle.dump(lig_dict, open('lig_dict.p', 'wb'))
+    pass
---
+++ data_processor/binding_pocket_filter.py
@@ -0,0 +1,48 @@
+"""
+    Takes output of `binding_pocket_analyse` with
+    RNA/Protein residue counts for various sphere sizes
+    and filters given a min RNA concentration and distance cutoff.
+"""
+from collections import defaultdict
+import pickle
+
+def ligand_filter(name):
+    if name in ['IRI', 'UNX']:
+        return False
+    return True
+
+def get_valids(d, max_dist, min_conc, min_size=4):
+    num_OK = 0
+    ok_ligs = defaultdict(list) 
+    for pdb, ligands in d.items():
+        for lig_id,lig_cuts in ligands:
+            lig_name = lig_id.split(":")[1]
+            for c in lig_cuts:
+                tot = c['rna'] + c['protein']
+                if tot == 0:
+                    continue
+                if c['rna'] < min_size:
+                    continue
+                rna_conc = c['rna'] / tot
+                if (rna_conc >= min_conc and c['cutoff'] == max_dist) and ligand_filter(lig_name):
+                    num_OK += 1
+                    ok_ligs[pdb].append(lig_id)
+                    continue
+
+    return ok_ligs
+
+def ligs_to_txt(d, dest="../data/ligs.txt"):
+    """
+        Write selected ligands to text file for Chimera.
+    """
+    with open(dest, "w") as o:
+        for pdb, ligs in d.items():
+            o.write(" ".join([pdb, *ligs]) + "\n")
+    pass
+if __name__ == "__main__":
+    d = pickle.load(open('../data/lig_dict.p', 'rb'))
+    c = 10
+    conc = .8
+    ligs = get_valids(d, c, conc)
+    print(ligs)
+    # ligs_to_txt(ligs)
